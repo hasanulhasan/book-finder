2483a373e99bbc25f8c1d8a27fa315c29897d058
hasanulhasan <hasanlink71@yahoo.com>
2024-01-18 16:49:00 +0600

    book project initiated

---
+++ src/component/Home.jsx
@@ -0,0 +1,15 @@
+import Books from "./Books";
+import Footer from "./Footer";
+import Navbar from "./Navbar";
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Books/>
+      <Footer/>
+    </div>
+  );
+};
+
+export default Home;
